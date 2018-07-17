8197fc6a5863f0c0266989e36c8682e9e6dda039
Carlos Reyes <carlos.reyes@ocapital.mx>
2018-07-16 21:55:15 -0500

    User creation login is working!

--- src/types/schema.d.ts
+++ src/types/schema.d.ts
@@ -1,43 +1,40 @@
 // tslint:disable
 // graphql typescript definitions
+export namespace GQL {
+  export interface IGraphQLResponseRoot {
+    data?: IQuery | IMutation;
+    errors?: Array<IGraphQLResponseError>;
+  }
 
-declare namespace GQL {
-interface IGraphQLResponseRoot {
-data?: IQuery | IMutation;
-errors?: Array<IGraphQLResponseError>;
-}
+  export interface IGraphQLResponseError {
+    /** Required for all errors */
+    message: string;
+    locations?: Array<IGraphQLResponseErrorLocation>;
+    /** 7.2.2 says 'GraphQL servers may provide additional entries to error' */
+    [propName: string]: any;
+  }
 
-interface IGraphQLResponseError {
-/** Required for all errors */
-message: string;
-locations?: Array<IGraphQLResponseErrorLocation>;
-/** 7.2.2 says 'GraphQL servers may provide additional entries to error' */
-[propName: string]: any;
-}
+  export interface IGraphQLResponseErrorLocation {
+    line: number;
+    column: number;
+  }
 
-interface IGraphQLResponseErrorLocation {
-line: number;
-column: number;
-}
+  export interface IQuery {
+    __typename: "Query";
+    hello: string;
+  }
 
-interface IQuery {
-__typename: "Query";
-hello: string;
-}
+  export interface IHelloOnQueryArguments {
+    name?: string | null;
+  }
 
-interface IHelloOnQueryArguments {
-name?: string | null;
-}
+  export interface IMutation {
+    __typename: "Mutation";
+    register: boolean | null;
+  }
 
-interface IMutation {
-__typename: "Mutation";
-register: boolean | null;
+  export interface IRegisterOnMutationArguments {
+    email: string;
+    password: string;
+  }
 }
-
-interface IRegisterOnMutationArguments {
-email: string;
-password: string;
-}
-}
-
-// tslint:enable
